4bf3674ac8b08262e3edc594a70cee802c2fa09c
zmmwl <zmmwl@aliyun.com>
2019-04-11 15:43:27 +0800

    zmm

--- cmd/geth/main.go
+++ cmd/geth/main.go
@@ -261,6 +261,7 @@ func geth(ctx *cli.Context) error {
 	if args := ctx.Args(); len(args) > 0 {
 		return fmt.Errorf("invalid command: %q", args[0])
 	}
+	fmt.Println("zmm: ", "node starting")
 	node := makeFullNode(ctx)
 	startNode(ctx, node)		//zmm: geth->startNodde()
 	node.Wait()
--- cmd/utils/flags.go
+++ cmd/utils/flags.go
@@ -1267,7 +1267,7 @@ func RegisterEthService(stack *node.Node, cfg *eth.Config) {
 		})
 	} else {
 		err = stack.Register(func(ctx *node.ServiceContext) (node.Service, error) {
-			fullNode, err := eth.New(ctx, cfg)	//zmm: stack.Register( eth.New() )
+			fullNode, err := eth.New(ctx, cfg)	//zmm: stack.Register( eth.New() ) Ethereum service creation
 			if fullNode != nil && cfg.LightServ > 0 {
 				ls, _ := les.NewLesServer(fullNode, cfg)
 				fullNode.AddLesServer(ls)
